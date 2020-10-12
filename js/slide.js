3e7536b142bc6b4d52e3d3e080906b781d357d20
Aleksandr Pryanikov <a.pryanikov@truemachine.ru>
2020-10-12 18:19:46 +0400

    open slide mnu

---
+++ js/slide.js
@@ -0,0 +1,11 @@
+const mobileMnu = document.querySelector(".mobileMnu");
+const open = document.querySelector(".open");
+const exit = document.querySelector(".exit");
+
+open.addEventListener('click', toggleChecked);
+exit.addEventListener('click', toggleChecked);
+
+
+function toggleChecked (e) {
+    mobileMnu.classList.toggle('hide');
+}
